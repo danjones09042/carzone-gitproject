af18b2ba82a344d60878f9f0e87fe60f3ac9dbd0
danjones09042 <danjones09042@gmail.com>
2022-10-13 19:39:53 +0200

    Added cars app and fetched the data into homepage

--- carzone/settings.py
+++ carzone/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
